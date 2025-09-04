2a52d1888bf118f663c682a1e2bc13c0975c6a4d
WataXMountDaun <golss9922@gmail.com>
2025-09-04 17:02:24 +0800

    Update Loader.lua

--- Loader/Loader.lua
+++ Loader/Loader.lua
@@ -1,2 +1,15 @@
+local token = "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"
 local url = "https://raw.githubusercontent.com/WataXMountDaun/MountDaun/main/Loader/main.lua"
-loadstring(game:HttpGetAsync(url))()
+
+local request = syn.request or http_request or request
+local response = request({
+    Url = url,
+    Method = "GET",
+    Headers = {["Authorization"] = "token "..token}
+})
+
+if response.StatusCode == 200 then
+    loadstring(response.Body)()
+else
+    warn("Gagal load script: "..response.StatusCode)
+end
