b048d109f508408e20140b7f3e267ebbca7e2796
WataXMountDaun <golss9922@gmail.com>
2025-09-04 17:08:11 +0800

    Update Loader.lua

--- Loader/Loader.lua
+++ Loader/Loader.lua
@@ -1,15 +1,4 @@
 local token = "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"
-local url = "https://raw.githubusercontent.com/WataXMountDaun/MountDaun/main/Loader/main.lua"
-
-local request = syn.request or http_request or request
-local response = request({
-    Url = url,
-    Method = "GET",
-    Headers = {["Authorization"] = "token "..token}
-})
-
-if response.StatusCode == 200 then
-    loadstring(response.Body)()
-else
-    warn("Gagal load script: "..response.StatusCode)
-end
+local url = "https://raw.githubusercontent.com/WataXMountDaun/MountDaun/main/Loader/Loader.lua"
+local response = syn.request({Url = url, Method = "GET", Headers = {["Authorization"] = "token "..token}})
+loadstring(response.Body)()
