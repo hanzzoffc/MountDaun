78d0827c54e4170abb9857a45156c8d1e12aed11
WataXMountDaun <golss9922@gmail.com>
2025-09-04 17:10:45 +0800

    Update Loader.lua

--- Loader/Loader.lua
+++ Loader/Loader.lua
@@ -1,4 +1,2 @@
-local token = "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"
-local url = "https://raw.githubusercontent.com/WataXMountDaun/MountDaun/main/Loader/Loader.lua"
-local response = syn.request({Url = url, Method = "GET", Headers = {["Authorization"] = "token "..token}})
-loadstring(response.Body)()
+local url = "https://raw.githubusercontent.com/WataXMountDaun/MountDaun/main/Loader/main.lua"
+loadstring(game:HttpGetAsync(url))()
